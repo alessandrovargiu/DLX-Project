1ce82619a48eeffaf0f1d706334f8f516c851940
Marco Chiarle s314730 <s314730@studenti.polito.it>
2023-09-06 12:17:33 +0200

    working on design and test PROAlu

--- Modules/datapath/ALUComponents/mux41_Generic.vhd
+++ Modules/datapath/ALUComponents/mux41_Generic.vhd
@@ -4,32 +4,32 @@ use WORK.constants.all;
 
 entity mux41_GENERIC is
 		Generic	(	
-			Nbit: integer:= 31
+			Nbit: integer:= 32
 		);
 		Port 	(	
-					A:		In	std_logic_vector(N-1 downto 0);
-					B:		In	std_logic_vector(N-1 downto 0);
-					C:		In	std_logic_vector(N-1 downto 0);
-					D:		In	std_logic_vector(N-1 downto 0);
+					A:		In	std_logic_vector(Nbit-1 downto 0);
+					B:		In	std_logic_vector(Nbit-1 downto 0);
+					C:		In	std_logic_vector(Nbit-1 downto 0);
+					D:		In	std_logic_vector(Nbit-1 downto 0);
 					SEL:	In	std_logic_vector(1 downto 0);
-					E:		Out	std_logic_vector(N-1 downto 0));
+					E:		Out	std_logic_vector(Nbit-1 downto 0));
 end mux41_GENERIC;
 
 architecture BEH of MUX41_GENERIC is
 
 begin
 
-		process (SHIFTER_OUT, ADD_OUT, CMP_OUT, LOGICALS_OUT, SEL)
+		process (A, B, C, D, SEL)
 		begin
 		case SEL is
 			when "00" =>
-				Y<=A; --add
+				E<=A; --add
 			when "01" =>
-				Y<=B; --shifter
+				E<=B; --shifter
 			when "10" =>
-				Y<=C; --comparator
+				E<=C; --comparator
 			when others =>
-				Y<=D; --logic
+				E<=D; --logic
 		end case;
 		end process;
 
